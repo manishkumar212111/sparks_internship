13ba9b12dca996d5c011abedbc52bac7720014ff
manishkumar212111 <manish.kumar212111@gmail.com>
2018-02-28 21:39:15 +0530

    test

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template
-
+import sqlite3
 app = Flask(__name__)\
 
 @app.route('/')
@@ -13,6 +13,14 @@ def resume():
 @app.route('/credit')
 def credit():
     return render_template('main.html')
+
+@app.route('/view_all_user')
+def view_all_user():
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+    cur.execute('select * from users')
+    a = cur.fetchall()
+    return render_template('view_all_user.html',result=a)
 
 
 
