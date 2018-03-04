e9a8efe974d6b6e66b49b15e63d783510ed41eac
manishkumar212111 <manish.kumar212111@gmail.com>
2018-03-04 14:11:12 +0530

    test

--- database.py
+++ database.py
@@ -1,17 +1,8 @@
 import sqlite3
 
 conn = sqlite3.connect('database.db')
-'''with sqlite3.connect("database.db") as con:
-    cur = con.cursor()
-    cur.execute("INSERT INTO users (name,email,credit)    VALUES(?, ?, ?)",("Shalini singh","Shalinisingh002@gmail.com",500) )
+print "Opened database successfully";
 
-    con.commit()
-    msg = "Record successfully added"
-    print msg
-    '''
-email="manish.kumar212111@gmail.com"
-cur = conn.cursor()
-a=cur.execute("select *from credit")
-a=cur.fetchall()
-print a
+conn.execute('insert into users values("Rakesh","rakesh@gmail.com","500")')
+print "Table created successfully";
 conn.close()
