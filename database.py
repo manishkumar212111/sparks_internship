6588a874f61f5e39b3fe8c89efa9ab56f6973edd
manishkumar212111 <manish.kumar212111@gmail.com>
2018-03-04 14:40:02 +0530

    test

--- database.py
+++ database.py
@@ -1,8 +1,17 @@
-import sqlite3
-
+import json,sqlite3
+import datetime
+val=datetime.date.today()
 conn = sqlite3.connect('database.db')
 print "Opened database successfully";
+cur = conn.cursor()
+now = datetime.datetime.now()
+date_string = now.strftime('%Y-%m-%d')
+fro="adkhfjsfh"
+to="sdfsfjf"
+amount=50
+cur.execute('insert into credit values(?,?,?,?)',[fro, to, amount, date_string])
+conn.commit()
 
-conn.execute('insert into users values("Rakesh","rakesh@gmail.com","500")')
-print "Table created successfully";
+cur.execute('select *from credit')
+print cur.fetchall()
 conn.close()
--- test.py
+++ test.py
@@ -1,21 +1,2 @@
-import json,requests
-url = "https://data.octagon58.hasura-app.io/v1/query"
-
-# This is the json payload for the query
-requestPayload = {
-    "type": "select",
-    "args": {
-        "table": "users",
 
 
-        }
-    }
-
-# Setting headers
-headers = {
-    "Content-Type": "application/json"
-}
-
-# Make the query and store response in resp
-resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
-print resp.content
